45e26d224874dfac4c77919e12ed658a56cb92ae
AliAlimohammadi89 <40763791+AliAlimohammadi89@users.noreply.github.com>
2019-03-13 22:00:55 +0330

    edit file controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+//new branche 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
