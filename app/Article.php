84594b8617fb8d5612fd1a9b2bb4a687ace36ce2
AliAlimohammadi89 <40763791+AliAlimohammadi89@users.noreply.github.com>
2019-03-15 20:15:32 +0330

    commit for test 8:14

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    protected $fillable = ['user_id' , 'title' , 'body'];
+    protected $fillable = ['user_id' , 'title' , 'body'];//for dont used in databased injection
 }
